2d4ac32fa66d6dfc1275176cbc24ea80463ff060
user <user@userdemacbook-pro.local>
2016-09-06 19:44:23 +0800

    Initial Commit

---
+++ EBOT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EBOT
+//
+//  Created by user on 16/9/6.
+//  Copyright © 2016年 yien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
